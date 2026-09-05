7c6a3fb10ff4193e3b5774195f3aab1acf675ad2
Lukáš "WQON" Nox <hi@wqon.dev>
2026-09-05 10:01:24 +0200

    configure Astro and the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  trailingSlash: "ignore",
+
+  server: {
+    port: 6363,
+  },
+
+  integrations: [mdx()],
+});
